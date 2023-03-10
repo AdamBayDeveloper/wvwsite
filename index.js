196cbe70130398ad6d9f680d4a69f1d70f03c146
Adam <adam@CHKRM072.local>
2023-03-10 17:20:57 +0100

    Make galery for photos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Javascript
 
 //Button: "Mehr Infos"
-var Text = 'Hallo'
+var Text = 'Für genauere Details über uns den "Unsere Abenteuer" knopf anclicken.'
 function myFunction() {
   document.getElementById("W1").innerHTML = Text;
 } 
